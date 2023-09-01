2d9dde5bcdecb8154958c2f0d3961928e1bc29c2
laxman <ramlaxman947@gmail.com>
2023-09-01 20:27:48 +0530

    UI changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -10,87 +10,93 @@ import {
   removeItem,
 } from "../redux/cartSlice";
 import Image from "next/image";
-import NoData from "../../../components/noData";
+import NoData from "../../components/noData";
+import Nav from "@/components/nav";
 
 const Cart = () => {
   const { cartItems, favItems } = useSelector((state: any) => state.cart);
   const dispatcher = useDispatch();
 
   return (
-    <div className="">
-      {cartItems.length > 0 ? (
-        cartItems.map((product: any) => {
-          return (
-            <div
-              className="text-slate-100 cartCard flex items-center justify-between relative"
-              key={product.id}
-            >
+    <>
+      <nav className="px-6 py-4">
+        <Nav />
+      </nav>
+      <div className="">
+        {cartItems.length > 0 ? (
+          cartItems.map((product: any) => {
+            return (
               <div
-                className="absolute top-1 right-2"
-                onClick={() => {
-                  !favItems.some((e: productType) => e.id === product.id)
-                    ? dispatcher(addFav(product))
-                    : dispatcher(removeFav(product));
-                }}
+                className="text-slate-100 cartCard flex items-center justify-between relative"
+                key={product.id}
               >
-                {favItems.some((e: productType) => e.id === product.id)
-                  ? "❤️"
-                  : "🤍"}
-              </div>
-              <Image
-                height={100}
-                width={100}
-                src={product.image}
-                alt=""
-                className="my-2"
-              />
-              <div className="text-center text-black text-sm">
-                {product.title}
-              </div>
-              <div className="text-center text-black font-bold mx-2">
-                ₹ {Number(product.price).toFixed(2)}
-              </div>
-              <div className="mx-2 text-black text-sm w-75 flex gap-2">
-                <label className="" htmlFor="qty">
-                  Quantity:
-                </label>
-                <input
-                  id="qty"
-                  value={product.quantity}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                    if (e.target.value > product.quantity) {
-                      dispatcher(increaseQnty(product));
-                    } else if (e.target.value < product.quantity) {
-                      dispatcher(decreaseQnty(product));
-                      // (e.target.value == "0" || product.quantity == "0") &&
-                      //   dispatcher(removeItem(product));
-                    }
-                  }}
-                  className="w-10 rounded"
-                  type="number"
-                  step={1}
-                  min={1}
-                />
-              </div>
-              <div className="">
-                <button
-                  className="btnAdd"
+                <div
+                  className="absolute top-1 right-2"
                   onClick={() => {
-                    dispatcher(removeItem(product));
+                    !favItems.some((e: productType) => e.id === product.id)
+                      ? dispatcher(addFav(product))
+                      : dispatcher(removeFav(product));
                   }}
                 >
-                  Remove
-                </button>
+                  {favItems.some((e: productType) => e.id === product.id)
+                    ? "❤️"
+                    : "🤍"}
+                </div>
+                <Image
+                  height={100}
+                  width={100}
+                  src={product.image}
+                  alt=""
+                  className="my-2"
+                />
+                <div className="text-center text-black text-sm">
+                  {product.title}
+                </div>
+                <div className="text-center text-black font-bold mx-2">
+                  ₹ {Number(product.price).toFixed(2)}
+                </div>
+                <div className="mx-2 text-black text-sm w-75 flex gap-2">
+                  <label className="" htmlFor="qty">
+                    Quantity:
+                  </label>
+                  <input
+                    id="qty"
+                    value={product.quantity}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      if (e.target.value > product.quantity) {
+                        dispatcher(increaseQnty(product));
+                      } else if (e.target.value < product.quantity) {
+                        dispatcher(decreaseQnty(product));
+                        // (e.target.value == "0" || product.quantity == "0") &&
+                        //   dispatcher(removeItem(product));
+                      }
+                    }}
+                    className="w-10 rounded"
+                    type="number"
+                    step={1}
+                    min={1}
+                  />
+                </div>
+                <div className="">
+                  <button
+                    className="btnAdd"
+                    onClick={() => {
+                      dispatcher(removeItem(product));
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })
-      ) : (
-        <>
-          <NoData pageName="cart" />
-        </>
-      )}
-    </div>
+            );
+          })
+        ) : (
+          <>
+            <NoData pageName="cart" />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,47 +1,53 @@
 "use client";
 import { useDispatch, useSelector } from "react-redux";
 import { addFav, removeFav, removeItem } from "../redux/cartSlice";
-import ProductImage from "../../../components/productImage";
-import NoData from "../../../components/noData";
+import ProductImage from "../../components/productImage";
+import NoData from "../../components/noData";
+import Nav from "@/components/nav";
 
 const Favorites = () => {
   const favItems = useSelector((state: any) => state.cart.favItems);
   const dispatcher = useDispatch();
 
   return (
-    <div className="flex flex-wrap">
-      {favItems.length > 0 ? (
-        favItems.map((product: any) => {
-          return (
-            <div className="text-slate-100 card relative" key={product.id}>
-              <div
-                className="absolute top-1 right-2"
-                onClick={() => {
-                  !favItems.some((e: any) => e.id === product.id)
-                    ? dispatcher(addFav(product))
-                    : dispatcher(removeFav(product));
-                }}
-              >
-                {favItems.some((e: any) => e.id === product.id) ? "❤️" : "🤍"}
+    <>
+      <nav className="px-6 py-4">
+        <Nav />
+      </nav>
+      <div className="flex flex-wrap">
+        {favItems.length > 0 ? (
+          favItems.map((product: any) => {
+            return (
+              <div className="text-slate-100 card relative" key={product.id}>
+                <div
+                  className="absolute top-1 right-2"
+                  onClick={() => {
+                    !favItems.some((e: any) => e.id === product.id)
+                      ? dispatcher(addFav(product))
+                      : dispatcher(removeFav(product));
+                  }}
+                >
+                  {favItems.some((e: any) => e.id === product.id) ? "❤️" : "🤍"}
+                </div>
+                {/* <div className="absolute top-1 right-2">🤍❤️</div> */}
+                {/* <Image src={product.image} alt="" className="mx-auto my-2" /> */}
+                <ProductImage product={product} fill={true} />
+                <div className="text-center text-black mt-5 text-sm">
+                  {product.title}
+                </div>
+                <div className="text-center text-black mt-3 mb-9 font-bold">
+                  ₹ {Number(product.price).toFixed(2)}
+                </div>
               </div>
-              {/* <div className="absolute top-1 right-2">🤍❤️</div> */}
-              {/* <Image src={product.image} alt="" className="mx-auto my-2" /> */}
-              <ProductImage product={product} fill={true} />
-              <div className="text-center text-black mt-5 text-sm">
-                {product.title}
-              </div>
-              <div className="text-center text-black mt-3 mb-9 font-bold">
-                ₹ {Number(product.price).toFixed(2)}
-              </div>
-            </div>
-          );
-        })
-      ) : (
-        <>
-          <NoData pageName="favorite" />
-        </>
-      )}
-    </div>
+            );
+          })
+        ) : (
+          <header className="mx-auto">
+            <NoData pageName="favorite" />
+          </header>
+        )}
+      </div>
+    </>
   );
 };
 
