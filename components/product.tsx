972c1a9060e72e8be86d736e86ec2b6271c1b79f
laxman <ramlaxman947@gmail.com>
2024-01-26 10:24:21 +0530

    Product item changes

--- components/product.tsx
+++ components/product.tsx
@@ -22,81 +22,89 @@ const Product = ({ product, page }: propsTypes) => {
   const { favItems, cartItems } = useSelector((state: any) => state.cart);
 
   return (
-    <>
-      <div
-        onClick={() => {
-          dispatcher(setSelectedProduct(product));
-          dispatcher(setProductModalOpen(true));
-        }}
-        className={`p-5 rounded border hover:scale-105 transition-transform ease-out duration-200 text-slate-100 card relative cursor-pointer  ${
-          page === "landing" ? "text-white hover:text-black" : ""
+    <button
+      type="button"
+      onClick={() => {
+        dispatcher(setSelectedProduct(product));
+        dispatcher(setProductModalOpen(true));
+      }}
+      className={`p-5 rounded border transition-transform ease-out duration-200 text-slate-100 card relative cursor-pointer  ${
+        page === "landing" ? "text-white hover:text-black" : ""
+      }`}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          return false;
+        }
+      }}
+    >
+      <button
+        type="button"
+        className={`${
+          page === "landing" ? "hidden" : "absolute top-1 right-2"
         }`}
+        onClick={(e) => {
+          e.stopPropagation();
+          !favItems.some((e: productType) => e.id === product.id)
+            ? dispatcher(addFav(product))
+            : dispatcher(removeFav(product));
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            return false;
+          }
+        }}
       >
-        <div
-          className={`${
-            page === "landing" ? "hidden" : "absolute top-1 right-2"
-          }`}
+        {favItems.some((e: productType) => e.id === product.id) ? "❤️" : "🤍"}
+      </button>
+      <div className="max-h-72 flex justify-center">
+        <ProductImage product={product} fill />
+      </div>
+      <div
+        className={`text-center text-black mt-5 text-sm max-w-[100] truncate`}
+      >
+        {product.title}
+      </div>
+      {page !== "landing" ? (
+        <div className="flex align-items-center">
+          <div className="text-center text-black mt-3 mb-9 font-bold">
+            ₹ {Number(product.price).toFixed(2)}
+          </div>
+        </div>
+      ) : null}
+      <div className={` ${page === "landing" ? "hidden" : "flex justify-end"}`}>
+        <button
+          className="btnAdd"
+          disabled={isAdded}
           onClick={(e) => {
             e.stopPropagation();
-            !favItems.some((e: productType) => e.id === product.id)
-              ? dispatcher(addFav(product))
-              : dispatcher(removeFav(product));
+            setIsAdded(true);
+            setTimeout(() => {
+              if (
+                cartItems.some((item: productType) => item.id === product.id)
+              ) {
+                dispatcher(increaseQnty(product));
+                setIsAdded(false);
+              } else {
+                const newProduct: any = {
+                  category: product.category,
+                  description: product.description,
+                  id: product.id,
+                  image: product.image,
+                  price: product.price,
+                  quantity: 1,
+                  rating: product.rating,
+                  title: product.title,
+                };
+                dispatcher(addItem(newProduct));
+                setIsAdded(false);
+              }
+            }, 1000);
           }}
         >
-          {favItems.some((e: productType) => e.id === product.id) ? "❤️" : "🤍"}
-        </div>
-        <div className="max-h-72 flex justify-center">
-          <ProductImage product={product} fill />
-        </div>
-        <div
-          className={`text-center text-black mt-5 text-sm max-w-[100] truncate`}
-        >
-          {product.title}
-        </div>
-        {page !== "landing" ? (
-          <div className="flex align-items-center">
-            <div className="text-center text-black mt-3 mb-9 font-bold">
-              ₹ {Number(product.price).toFixed(2)}
-            </div>
-          </div>
-        ) : null}
-        <div
-          className={` ${page === "landing" ? "hidden" : "flex justify-end"}`}
-        >
-          <button
-            className="btnAdd"
-            disabled={isAdded}
-            onClick={(e) => {
-              e.stopPropagation();
-              setIsAdded(true);
-              setTimeout(() => {
-                if (
-                  cartItems.some((item: productType) => item.id === product.id)
-                ) {
-                  dispatcher(increaseQnty(product));
-                  setIsAdded(false);
-                } else {
-                  const newProduct: any = {
-                    category: product.category,
-                    description: product.description,
-                    id: product.id,
-                    image: product.image,
-                    price: product.price,
-                    quantity: 1,
-                    rating: product.rating,
-                    title: product.title,
-                  };
-                  dispatcher(addItem(newProduct));
-                  setIsAdded(false);
-                }
-              }, 1000);
-            }}
-          >
-            {!isAdded ? "Add to cart" : "Added"}
-          </button>
-        </div>
+          {!isAdded ? "Add to cart" : "Added"}
+        </button>
       </div>
-    </>
+    </button>
   );
 };
 
--- components/productImage.tsx
+++ components/productImage.tsx
@@ -19,7 +19,7 @@ const ProductImage = ({ product, fill }: props) => {
           alt={product.title}
           className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
             loading
-              ? "scale-110 blur-2xl grayscale"
+              ? "scale-105 blur-2xl grayscale"
               : "scale-100 blur-0 grayscale-0"
           }`}
           onLoadingComplete={() => setLoading(false)}
@@ -32,7 +32,7 @@ const ProductImage = ({ product, fill }: props) => {
           alt={product.title}
           className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
             loading
-              ? "scale-110 blur-2xl grayscale"
+              ? "scale-105 blur-2xl grayscale"
               : "scale-100 blur-0 grayscale-0"
           }`}
           onLoadingComplete={() => setLoading(false)}
