c7be704e212967b2aead3fe0b234ada3db347f8c
laxman <ramlaxman947@gmail.com>
2024-09-28 08:23:45 +0530

    Emmets

---
+++ components/emmet.tsx
@@ -0,0 +1,17 @@
+//  -->2 .className#idName ---
+//  -->3 .container>header.header
+
+const Emmet = () => {
+  return (
+    <div>
+      <div className="mainContainer" id="main">
+        2
+      </div>
+      <div className="container">
+        <header className="header">3</header>
+      </div>
+    </div>
+  );
+};
+
+export default Emmet;
