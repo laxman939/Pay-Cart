a6dfada676560ee411a20ee4ed75c9e6a56cbec1
laxman <ramlaxman947@gmail.com>
2025-01-03 18:51:18 +0530

    Added styles to scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LandingPage from "@/components/landing/landingPage";
 export default async function Page() {
   return (
     <>
-      <main className="flex items-center justify-between flex-wrap">
+      <main className="flex items-center justify-between flex-wrap main_page">
         <LandingPage />
       </main>
     </>
